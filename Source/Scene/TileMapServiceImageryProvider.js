fbfc4500de6a5853aefb9c9628c99b241396c89d
gcebelieu <gilles.cebelieu@ign.fr>
2014-07-29 21:52:53 +0200

    renaming WMTSImageryProvider into WebMapTileServiceImageryProvider

--- Source/Scene/TileMapServiceImageryProvider.js
+++ Source/Scene/TileMapServiceImageryProvider.js
@@ -63,7 +63,7 @@ define([
      * @see BingMapsImageryProvider
      * @see GoogleEarthImageryProvider
      * @see OpenStreetMapImageryProvider
-     * @see WMTSImageryProvider
+     * @see WebMapTileServiceImageryProvider
      * @see SingleTileImageryProvider
      * @see WebMapServiceImageryProvider
      *
